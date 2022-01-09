fc26fba395a749330dd3d358e6f2198bd86e78db
Danila Fomin <dangdancheg@gmail.com>
2022-01-09 16:37:07 +0300

    .

--- x.py
+++ x.py
@@ -1,8 +1,18 @@
+import os
+import shutil
+
+import pika
+import requests
+
 print(1)
 print('вася')
 print(True)
 
 class Duck:
-    isDuck = True
+    def __init__(self, name):
+        self.name = name
 
-print(Duck)
+    def __str__(self):
+        return 'Я утка по имени ' + self.name
+
+print(Duck('cola'))
