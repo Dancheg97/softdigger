b9cb60c002272e0ba8344154b699d5bcb871b627
Danila Fomin <dangdancheg@gmail.com>
2022-01-10 21:56:59 +0300

    .

--- runner.py
+++ runner.py
@@ -5,8 +5,7 @@ print("starting updating from git")
 
 while True:
     time.sleep(0.2)
-    os.system("git pull &> /dev/null")
-    rez = os.system("git status -uno &> /dev/null")
-    if rez != 0:
+    rez = os.popen("git pull").read()
+    if not "Already up to date" in rez:
         print("making an update")
         os.system("python3 -m mkdocs build")
