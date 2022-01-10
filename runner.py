01b98c8d3acfd4c713d328171b72ff7f6a838b87
Danila Fomin <dangdancheg@gmail.com>
2022-01-10 17:07:57 +0300

    .

--- runner.py
+++ runner.py
@@ -5,6 +5,6 @@ while True:
     time.sleep(0.2)
     os.system("git pull &> /dev/null")
     rez = os.system("git status -uno &> /dev/null")
-    if rez is not 0:
+    if rez != 0:
         print("making an update")
         os.system("python3 -m mkdocs build")
