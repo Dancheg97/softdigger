ac540847ddaf36e998a9fff912283515b586e87a
Danila Fomin <dangdancheg@gmail.com>
2022-01-10 13:40:58 +0300

    .

--- debug.py
+++
@@ -1,7 +0,0 @@
-A = 1
-B = 2
-C = "coca cola"
-D = "fanta"
-B = True
-A = "olas"
-print(f"{A} {B} {C} {D}")
---
+++ func.py
@@ -0,0 +1,6 @@
+"""file with dangerous function"""
+
+
+def double_capitalize(inp: str) -> str:
+    """funciton that capitilizes and doubles string"""
+    return inp.upper() * 2
