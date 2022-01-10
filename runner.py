0db0265198c940952fad5322d4b7fdb493d6d96d
Danila Fomin <dangdancheg@gmail.com>
2022-01-10 16:54:24 +0300

    .

--- runner.py
+++ runner.py
@@ -3,5 +3,7 @@ import time
 
 while True:
     time.sleep(60)
-    os.system('git pull')
-    os.system('python3 -m mkdocs build')
+    os.system("git pull")
+    rez = os.system("git status -uno")
+    if rez is not 0:
+        os.system("python3 -m mkdocs build")
