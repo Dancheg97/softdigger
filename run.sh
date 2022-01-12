656740fbc35753d615884c7230d98561033a7ebb
Danila Fomin <dangdancheg@gmail.com>
2022-01-12 13:56:33 +0300

    .

--- run.sh
+++ run.sh
@@ -2,8 +2,6 @@ clear
 # check all installed
 git --version
 python3.7 --version
-# kill 80 port process
-kill $(lsof -t -i:80)
 # paths
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:/usr/local/bin/python3.7
